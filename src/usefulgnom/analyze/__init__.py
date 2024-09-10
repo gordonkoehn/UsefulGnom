8c04e1017e7112ac414c660a2d28489399690df8
Gordon J. Köhn <gordon@koehn.net>
2024-09-10 15:58:15 +0200

    init fix

--- src/usefulgnom/analyze/__init__.py
+++ src/usefulgnom/analyze/__init__.py
@@ -4,7 +4,4 @@ from usefulgnom.analyze.basecnt_coverage import run_basecnt_coverage
 from usefulgnom.analyze.total_coverage import run_total_coverage_depth
 
 
-__all__ = [
-    "run_basecnt_coverage"
-    "run_total_coverage_depth"
-    ]
+__all__ = ["run_basecnt_coverage", "run_total_coverage_depth"]
--- src/usefulgnom/serialize/__init__.py
+++ src/usefulgnom/serialize/__init__.py
@@ -4,7 +4,7 @@ from usefulgnom.serialize.coverage import extract_sample_ID
 from usefulgnom.serialize.basecnt_coverage import load_convert_bnc
 from usefulgnom.serialize.total_coverage import load_convert_total
 
-all = [
+__all__ = [
     "load_convert_bnc",
     "load_convert_total",
     "extract_sample_ID",
