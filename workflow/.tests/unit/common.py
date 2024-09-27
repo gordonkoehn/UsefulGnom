0aaf093d1aab73e86af03b0a062609aea4aaac44
Gordon J. Köhn <gordon@koehn.net>
2024-09-27 11:05:35 +0200

    reinstating order

--- workflow/.tests/unit/common.py
+++ workflow/.tests/unit/common.py
@@ -5,6 +5,7 @@ Common code for unit testing of rules generated with Snakemake 8.18.2.
 from pathlib import Path
 import subprocess as sp
 import os
+import pandas as pd
 
 import sys
 
@@ -74,3 +75,26 @@ class OutputChecker:
         Compare the generated file with the expected file.
         """
         sp.check_output(["cmp", generated_file, expected_file])
+
+
+def compare_csv_files(
+    file1_path: str, file2_path: str, tolerance: float = 1e-4
+) -> bool:
+    """
+    Compare two CSV files with a given tolerance.
+    """
+    df1 = pd.read_csv(file1_path, skiprows=[1])
+    df2 = pd.read_csv(file2_path, skiprows=[1])
+
+    if df1.shape != df2.shape:
+        raise ValueError("DataFrames have different shapes")
+
+    # check that the data frames contrain the same data types
+    assert df1.dtypes.equals(df2.dtypes)
+
+    # check that the data frames contain the same data
+    pd.testing.assert_frame_equal(
+        df1, df2, check_exact=False, rtol=tolerance, atol=tolerance
+    )
+
+    return True
